12b48b4b596a5c0ea02cc52d2cf0a7268eeaa36b
eddamnatihafsa <165345763+eddamnatihafsa@users.noreply.github.com>
2024-05-15 23:06:15 +0100

    Main

---
+++ Main.cpp
@@ -0,0 +1,33 @@
+#include "iostream"
+#include "Quiz.cpp"
+using namespace std;
+
+
+
+
+
+int main() {
+    int score = 0; // Score final ^_^
+
+    cout << "Bienvenue au quiz informatique ! bonne chance :)" << endl;
+    cout << "Répondez avec A, B, ou C." << endl;
+    // Champ de poser les questions
+    for (int i = 0; i < 3; ++i) {
+        cout << questions[i].question << endl;
+        char réponse_utilisateur;
+        cin >> réponse_utilisateur;
+
+        // Tester la réponse
+        if (toupper(réponse_utilisateur) == questions[i].bonne_reponse) {
+            cout << "Bonne réponse !" << endl;
+            ++score;
+        }
+        else {
+            cout << "Mauvaise réponse." << endl;
+        }
+    }
+
+    // Affichage du score final
+    cout << "Votre score final : " << score << " sur 9." << endl;
+
+    return 0;
